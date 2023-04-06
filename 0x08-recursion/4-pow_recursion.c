dd632100535ed151c2e9ad9259204f3867a35ae8
OkodiMiebiEdward <miebiokodiedward@gmail.com>
2023-04-05 18:41:59 -0700

    calculate square withoutstandard lib

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - value of x exponented to y
+ * @x: The base number
+ * @y: The base number will be raised to the powerr of y
+ *
+ * Return: result of the power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (0);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
