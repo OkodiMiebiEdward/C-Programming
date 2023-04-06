bdeadd01bfb30f51d86c5041b7839d949a3f01de
OkodiMiebiEdward <miebiokodiedward@gmail.com>
2023-04-06 07:08:11 -0700

    compare strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,11 +12,11 @@ int wildcmp(char *s1, char *s2)
 	/*condition statement for validation*/
 	if (*s1 == '\0')
 	{
-		if (*s2 != '\0' && *s2 == '\0')
+		if (*s2 != '\0' && *s2 == '*')
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
-		return (*s2 == '\0');
+		return (*s2 == '*');
 	}
 
 	if (*s2 == '*')
