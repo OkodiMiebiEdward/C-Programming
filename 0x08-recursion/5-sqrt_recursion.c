dd5636a566427d4e13141c175ddf023c089fa497
OkodiMiebiEdward <miebiokodiedward@gmail.com>
2023-04-05 19:01:17 -0700

    Square root of number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the square root of number
+ * @n: number whose square root will be looked at
+ *
+ * Return answer no be returned
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (0);
+}
