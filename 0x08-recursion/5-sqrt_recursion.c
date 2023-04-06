c672cb922e2b361729558c18014ecd8b0b21d6b1
OkodiMiebiEdward <miebiokodiedward@gmail.com>
2023-04-05 19:12:47 -0700

    square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,11 +4,32 @@
  * _sqrt_recursion - returns the square root of number
  * @n: number whose square root will be looked at
  *
- * Return answer no be returned
+ * Return: answer no be returned
  */
+
+int original_recursion_sqrt(int n, int i);
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (0);
+	return (original_recursion_sqrt(n, 0));
+}
+
+
+/**
+ * original_recursion_sqrt -  find the number recursively
+ * 
+ * @n: number whose square root is to be fetched
+ * @i: number of times to be moved
+ *
+ * Return: Actual square root
+ */
+int original_recursion_sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }
