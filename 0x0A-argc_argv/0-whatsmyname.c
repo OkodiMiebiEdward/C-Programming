e7279e53e099714c5829fe3793a8dc8b6a37ef0b
OkodiMiebiEdward <miebiokodiedward@gmail.com>
2023-04-09 01:52:30 -0700

    passed the argv argument to the command line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: total arguments
+ * @argv: collection of arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	/*print array that will be passed*/
+	printf("%s\n", *argv);
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Working with the argc and argv in C programming
