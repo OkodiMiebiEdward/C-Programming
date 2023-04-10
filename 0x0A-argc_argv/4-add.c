8c6ac869b6e527f32ac9d5479003061e54fd3f8c
OkodiMiebiEdward <miebiokodiedward@gmail.com>
2023-04-10 09:25:01 -0700

    program add two numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,68 @@
+#include <ctype.h>
+#include <string.h>
+#include "main.h"
+/**
+ * check_num - check for digit
+ * @str: array str
+ *
+ * Return: 0 on successful completion
+ */
+int check_num(char *str)
+{
+	
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - Prints name
+ * @argc: Counts arguments
+ * @argv: array
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc) 
+	{
+		if (check_num(argv[count]))
+
+		{
+			str_to_int = atoi(argv[count]); 
+			sum += str_to_int;
+		}
+
+	
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
+
