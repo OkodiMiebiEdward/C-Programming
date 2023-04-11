8483d682e9291c55db018ea319a8f23a869a7b4b
OkodiMiebiEdward <miebiokodiedward@gmail.com>
2023-04-11 10:11:40 -0700

    changed the conditional operator to get the correct output

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,13 +19,13 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 
 	x = y = 0;
-	while (s1[x] == '\0')
+	while (s1[x] != '\0')
 		x++;
 	while (s2[y] != '\0')
 		y++;
 	strResult = malloc(sizeof(char) * (x + y + 1));
 
-	if (strResult != NULL)
+	if (strResult == NULL)
 		return (NULL);
 	x = y = 0;
 	while (s1[x] != '\0')
