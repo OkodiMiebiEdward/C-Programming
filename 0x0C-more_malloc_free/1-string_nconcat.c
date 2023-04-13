dcec6093368b26e3cb4b5cb9bfc033577bc076fb
OkodiMiebiEdward <miebiokodiedward@gmail.com>
2023-04-13 07:19:57 -0700

    remove trailing spaces to enable betty compliancy

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,36 +12,28 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        char *result;
-        unsigned int x = 0, y = 0, stringA = 0, stringB = 0;
-
-        while (s1 && s1[stringA])
-                stringA++;
-        while (s2 && s2[stringB])
-                stringB++;
-
-        if (n < stringB)
-                result = malloc(sizeof(char) * (stringA + n + 1));
-        else
-                result = malloc(sizeof(char) * (stringA + stringB + 1));
-
-        if (!result)
-                return (NULL);
-
-        while (x < stringA)
-        {
-                result[x] = s1[x];
-                x++;
-        }
-
-        while (n < stringB && x < (stringA + n))
-                result[x++] = s2[y++];
-
-        while (n >= stringB && x < (stringA + stringB))
-                result[x++] = s2[y++];
-
-        result[x] = '\0';
-
-        return (result);
+	char *result;
+	unsigned int x = 0, y = 0, stringA = 0, stringB = 0;
+	while (s1 && s1[stringA])
+		stringA++;
+	while (s2 && s2[stringB])
+		stringB++;
+	if (n < stringB)
+		result = malloc(sizeof(char) * (stringA + n + 1));
+	else
+		result = malloc(sizeof(char) * (stringA + stringB + 1));
+	if (!result)
+		return (NULL);
+	while (x < stringA)
+	{
+		result[x] = s1[x];
+		x++;
+	}
+	while (n < stringB && x < (stringA + n))
+		result[x++] = s2[y++];
+	while (n >= stringB && x < (stringA + stringB))
+		result[x++] = s2[y++];
+	result[x] = '\0';
+	return (result);
 }
 
