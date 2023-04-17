de4b72ecb8fe552496adf557f489d60f430e3054
OkodiMiebiEdward <miebiokodiedward@gmail.com>
2023-04-17 04:47:50 -0700

    initialized varibles with struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - variables initailization
+ * @d: argument expected that points to the dog struct
+ * @name: the variable name to be initialized
+ * @age: argument of type float expected to be passed in this parameter
+ * @owner: the variable owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+	}
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
+
