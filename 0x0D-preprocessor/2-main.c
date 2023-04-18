710321ca355a75f910dd0f3e77d3cdb8d119e90d
OkodiMiebiEdward <miebiokodiedward@gmail.com>
2023-04-18 04:59:16 -0700

    function prints name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the application
+ * Return: success on 0
+ */
+int main(void)
+{
+	/*prints name of file*/
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
