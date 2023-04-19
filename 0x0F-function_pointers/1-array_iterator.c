b7e8593186badcbd56d12117f9f7b82dcec141f8
OkodiMiebiEdward <miebiokodiedward@gmail.com>
2023-04-19 06:33:15 -0700

    added space after declarion for betty compliancy

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,14 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	/*iterate through members*/
-	unsigned int i;
-	if (array == NULL || action == NULL)
-		return;
+	unsigned int i = 0;
 
-	for (i = 0; i < size; i++)
+	if (array == NULL || action == NULL)
+	{	
+		return;
+	}
+
+	for (; i < size; i++)
 	{
 		action(array[i]);
 	}
